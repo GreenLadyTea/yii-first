9e649209e1a2e916422b0026b1da0d841a475769
kudinovamo <kudinovamo@mer.ci.nsu.ru>
2021-11-23 11:30:22 +0700

    Initial commit

---
+++ config/db.php
@@ -0,0 +1,14 @@
+<?php
+
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'mysql:host=localhost;dbname=messagesdb',
+    'username' => 'debian-sys-maint',
+    'password' => 'YUOULJpihCP0s1xY',
+    'charset' => 'utf8',
+
+    // Schema cache options (for production environment)
+    //'enableSchemaCache' => true,
+    //'schemaCacheDuration' => 60,
+    //'schemaCache' => 'cache',
+];
